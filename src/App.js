1e3b1c168a673b87cb9df875242421d1cb75403b
mohamedmehdigara <garamehdi57@yahoo.com>
2023-09-18 10:00:37 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const BoardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 80px);
+  gap: 10px;
+  margin: 20px auto;
+  width: 600px;
+`;
+
+const Cell = styled.div`
+  width: 80px;
+  height: 80px;
+  background-color: ${props => props.color || 'white'};
+  border: 2px solid #333;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const App = () => {
+  const [board, setBoard] = useState(Array(7).fill(Array(6).fill(null)));
+  const [isRedTurn, setIsRedTurn] = useState(true);
+
+  const handleClick = (col) => {
+    const newBoard = [...board];
+    const columnIndex = newBoard[col].indexOf(null);
+    
+    if (columnIndex !== -1) {
+      newBoard[col][columnIndex] = isRedTurn ? 'red' : 'yellow';
+      setBoard(newBoard);
+      setIsRedTurn(!isRedTurn);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Connect Four</h1>
+      <BoardContainer>
+        {board.map((column, colIndex) => (
+          column.map((cellColor, rowIndex) => (
+            <Cell
+              key={`${colIndex}-${rowIndex}`}
+              color={cellColor}
+              onClick={() => handleClick(colIndex)}
+            />
+          ))
+        ))}
+      </BoardContainer>
     </div>
   );
-}
+};
 
 export default App;
